537a90b5531d5553c1969a2e3fda24a033a6e528
StefanieVW <stefanie.veronica@student.umn.ac.id>
2024-09-17 17:34:12 +0700

    34

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -12,11 +12,22 @@ val TABS_FIXED = listOf(
 )
 class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
+    val titles = listOf(
+        R.string.starbucks_title,
+        R.string.janjijiwa_title,
+        R.string.kopikenangan_title
+    )
+
+    val descriptions = listOf(
+        R.string.starbucks_desc,
+        R.string.janjijiwa_desc,
+        R.string.kopikenangan_desc
+    )
     override fun getItemCount(): Int {
-        return TABS_FIXED.size
+        return titles.size
     }
-    override fun createFragment(position: Int): Fragment
-    {
-        return CafeDetailFragment()
+
+    override fun createFragment(position: Int): Fragment {
+        return CafeDetailFragment.newInstance(titles[position], descriptions[position])
     }
 }
